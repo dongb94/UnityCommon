4b0c897ce560f4db9be9e6e05fdc9b1702654f3c
DONGGEON BYEON <dongb94@gmail.com>
2020-07-02 18:57:41 +0900

    Update GenericAnimationConverter
    # Update MotionTracker GUI

--- GenericAnimationConverter/Editor/MotionTrackerGUI.cs
+++ GenericAnimationConverter/Editor/MotionTrackerGUI.cs
@@ -14,15 +14,19 @@ public class MotionTrackerGUI : EditorWindow
     private float _scaleDelta;
     private float _showDistance;
 
+    private Transform _actor;
+    private Transform _tracker;
+
     private int _transformListSize;
-    private List<Transform> _tl;
+    private List<TransformPair> _trackList;
 
     #endregion
 
     private void Awake()
     {
+        Debug.Log("21212");
         _scaleDelta = 1f;
-        _tl = new List<Transform>();
+        _trackList = new List<TransformPair>();
     }
 
     [MenuItem("AnimationConverter/MotionTracker")]
@@ -40,8 +44,11 @@ public class MotionTrackerGUI : EditorWindow
     {
         EditorGUILayout.LabelField("MotionTracker");
         _scaleDelta = EditorGUILayout.Slider("Scale Delta Value", _scaleDelta, 0f, 500f);
-        _showDistance = EditorGUILayout.Slider("Model View Distance", _showDistance, 0f, 10f);
+        _showDistance = EditorGUILayout.Slider("Model View Distance", _showDistance, -10f, 10f);
 
+        _actor = EditorGUILayout.ObjectField("Actor", _actor, typeof(Transform), true) as Transform;
+        _tracker = EditorGUILayout.ObjectField("Tracker", _tracker, typeof(Transform), true) as Transform;
+        
         _transformListSize = EditorGUILayout.IntField("Track List Size", _transformListSize);
         
         ListSizeChange();
@@ -49,28 +56,49 @@ public class MotionTrackerGUI : EditorWindow
         using (var scrollView = new EditorGUILayout.ScrollViewScope(_scrollPos))
         {
             _scrollPos = scrollView.scrollPosition;
+            EditorGUI.indentLevel++;
             for (int i = 0; i < _transformListSize; i++)
             {
-                _tl[i] = EditorGUILayout.ObjectField("Transform", _tl[i], typeof(Transform), true) as Transform;
+                var toggleText = _trackList[i].UnTrack ? "No Tracking" : "Element";
+                _trackList[i].UnTrack = EditorGUILayout.ToggleLeft($" {toggleText} {i+1} -------------------------------------------------------------------------------------------------------------------", _trackList[i].UnTrack);
+                EditorGUI.indentLevel++;
+                using (var h = new EditorGUILayout.HorizontalScope())
+                {
+                    _trackList[i].From = EditorGUILayout.ObjectField("Actor Part Transform", _trackList[i].From, typeof(Transform), true, GUILayout.Width(330)) as Transform;
+                    EditorGUILayout.Space();
+                    _trackList[i].AdjustmentRotation = EditorGUILayout.Vector3Field("Adjustment Rotation", _trackList[i].AdjustmentRotation, GUILayout.Width(330));
+                    EditorGUILayout.Space();
+                }
+                using (var h = new EditorGUILayout.HorizontalScope())
+                {
+                    _trackList[i].To = EditorGUILayout.ObjectField("Tracker Part Transform", _trackList[i].To, typeof(Transform), true, GUILayout.Width(330)) as Transform;
+                    EditorGUILayout.Space();
+                    
+                    _trackList[i].AdjustmentPosition = EditorGUILayout.Vector3Field("Adjustment Position", _trackList[i].AdjustmentPosition,GUILayout.Width(330));
+                    EditorGUILayout.Space();
+                }
+                EditorGUILayout.Space();
+                EditorGUI.indentLevel--;
             }
+            EditorGUI.indentLevel--;
         }
     }
 
     private void ListSizeChange()
     {
-        if (_transformListSize == _tl.Count) return;
-        if (_transformListSize < _tl.Count)
+        if (_transformListSize == _trackList.Count) return;
+        if (_transformListSize < _trackList.Count)
         {
-            while (_transformListSize != _tl.Count)
+            while (_transformListSize != _trackList.Count)
             {
-                _tl.RemoveAt(_tl.Count - 1);
+                _trackList.RemoveAt(_trackList.Count - 1);
             }
         }
         else
         {
-            while (_transformListSize != _tl.Count)
+            while (_transformListSize != _trackList.Count)
             {
-                _tl.Add(null);
+                _trackList.Add(new TransformPair());
             }
         }
     }
