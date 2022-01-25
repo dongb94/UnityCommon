2c8aa054eabe4d88a1aac93a78a963b188ceee7e
DONGGEON BYEON <dongb94@gmail.com>
2022-01-25 15:32:39 +0900

    Add ObjectAnalysis

---
+++ ObjectAnalysis/PrintAttribute.cs
@@ -0,0 +1,113 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Almond.Network;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PrintAttribute : MonoBehaviour
+    {
+        struct TestSTRUCT
+        {
+            public int[] twest;
+        }
+
+        private void Start()
+        {
+            var exp = new ST_DELETE_MAIL_RECV();
+            exp.ucMailType = MailType.SystemMail;
+            exp.ullMailKey = new List<ulong>();
+            exp.packetList = new List<string>();
+            
+            exp.ullMailKey.Add(111);
+            exp.ullMailKey.Add(122);
+            exp.ullMailKey.Add(133);
+            
+            printValue(exp);
+            
+            TestSTRUCT aa = new TestSTRUCT();
+            aa.twest = new int[10];
+
+            // printValue(aa);
+        }
+
+        private bool IsStruct(Type t)
+        {
+            return (t.IsValueType && !t.IsEnum && !t.IsPrimitive);
+        }
+        
+        private string printValue(object notValue, string str = "")
+        {
+            var t = notValue.GetType();
+            if (t.IsValueType && !IsStruct(t)) // Value && Enum
+            {
+                Debug.Log($"{str}Value : \t{notValue}");
+            }
+            else if(t.IsArray)                                // Array
+            {
+                str += "\t";
+                foreach (var item in (Array) notValue)
+                {
+                    printValue(item, str);
+                }
+            }
+            else if(typeof(IList).IsAssignableFrom(t))        // List
+            {
+                str += "\t";
+                foreach (var item in (IList) notValue)
+                {
+                    printValue(item, str);
+                }
+            }
+            else if(typeof(IDictionary).IsAssignableFrom(t)) // Dictionary
+            {
+                str += "\t";
+                foreach (var item in (IDictionary) notValue)
+                {
+                    printValue(item, str);
+                }
+            }
+            else if(typeof(Queue).IsAssignableFrom(t)) // Queue
+            {
+                str += "\t";
+                foreach (var item in (Queue) notValue)
+                {
+                    printValue(item, str);
+                }
+            }
+            else if(typeof(Stack).IsAssignableFrom(t)) // Stack
+            {
+                str += "\t";
+                foreach (var item in (Stack) notValue)
+                {
+                    printValue(item, str);
+                }
+            }
+            else if(IsStruct(t) || t.IsClass) // Struct || Class
+            {
+                str += "\t";
+                var fieldInfos = t.GetFields();
+                foreach (var field in fieldInfos)
+                {
+                    Debug.Log($"{str}______________________________________________\n"
+                              +$"{str}Name : \t\t{field.Name}\n"
+                              +$"{str}FieldType : \t{field.FieldType}\n"
+                              +$"{str}IsClass : \t{field.FieldType.IsClass}\n"
+                              +$"{str}IsValueType : \t{field.FieldType.IsValueType}\n"
+                              +$"{str}IsEnum : \t{field.FieldType.IsEnum}\n"
+                              +$"{str}IsPrimitive : \t{field.FieldType.IsPrimitive}\n"
+                              +$"{str}----------------------------------------"
+                              );
+                    printValue(field.GetValue(notValue), str);
+                }
+            }
+
+            return str;
+        }
+        
+        
+        
+    }
+}
